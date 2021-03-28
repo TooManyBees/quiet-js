8dd0221a413c27f77d9e376ed3a4b18dde83580c
Jesse Bees <hi@toomanybees.com>
2021-03-28 15:26:22 -0400

    Let very tall info panels scroll better

--- client-svelte/src/Info.svelte
+++ client-svelte/src/Info.svelte
@@ -16,7 +16,8 @@
     bottom: 0;
     box-sizing: border-box;
     max-height: 100vh;
-    overflow-y: auto;
+    display: flex;
+    flex-direction: column;
   }
 
   header {
@@ -83,6 +84,10 @@
     width: 16px;
     height: 16px;
   }
+
+  .scroll {
+    overflow-y: scroll;
+  }
 </style>
 
 <div class="buttons">
@@ -103,48 +108,50 @@
       <CancelButton onclick={() => howToVisible = false} />
     </header>
 
-    <p>For rules, <a href="https://store.buriedwithoutceremony.com/collections/frontpage/products/the-quiet-year-pdf" target="_blank">get a copy</a>. Before playing, bring up <a href="https://buriedwithoutceremony.com/wp-content/uploads/2019/08/The-Quiet-Year-Oracle.pdf" target="_blank">The Oracle</a> for reference.
-    </p>
+    <div class="scroll">
+      <p>For rules, <a href="https://store.buriedwithoutceremony.com/collections/frontpage/products/the-quiet-year-pdf" target="_blank">get a copy</a>. Before playing, bring up <a href="https://buriedwithoutceremony.com/wp-content/uploads/2019/08/The-Quiet-Year-Oracle.pdf" target="_blank">The Oracle</a> for reference.
+      </p>
 
-    <p>
-      Hop on a voice chat with your friends. One person join a random room on this site, then share the link. The first person to click <em>Start game</em> takes the first turn. The game will shuffle a deck of 52 or 36 cards for you.
-    </p>
+      <p>
+        Hop on a voice chat with your friends. One person join a random room on this site, then share the link. The first person to click <em>Start game</em> takes the first turn. The game will shuffle a deck of 52 or 36 cards for you.
+      </p>
 
-    <p>
-      On your turn, click <em>Draw card</em> to draw a card. No one will see it except you, so announce it to the other players. When you're done, click <em>Pass turn</em>.
-    </p>
+      <p>
+        On your turn, click <em>Draw card</em> to draw a card. No one will see it except you, so announce it to the other players. When you're done, click <em>Pass turn</em>.
+      </p>
 
-    <p>
-      The game will handle <span class="card">KING_DIAMONDS</span> and reducing project dice on its own. It's on the players to remove projects when appropriate.
-    </p>
+      <p>
+        The game will handle <span class="card">KING_DIAMONDS</span> and reducing project dice on its own. It's on the players to remove projects when appropriate.
+      </p>
 
-    <p>
-      Anyone can draw, erase, or place projects at any time. Anyone can join the room at any time and get added to the turn order. Anyone can click <em>Pass turn</em> to end the current turn early and advance to the next player. The lack of guard rails is because this app is a facilitator, not a dungeon master. I only recommend playing with people you'd feel comfortable sitting across a table from.
-    </p>
-    <h2>Tools</h2>
-    <ul class="tools">
-      <li>
-        <img src="/icons/move-selector.svg" alt="Cross with arrows pointing in 4 directions">
-        Move the map around
-      </li>
-      <li>
-        <img src="/icons/pencil.svg" alt="pencil">
-        Draw on the map
-      </li>
-      <li>
-        <img src="/icons/eraser.svg" alt="eraser">
-        Erase on the map
-      </li>
-      <li>
-        <img src="/icons/zoom-in.svg" alt="magnifying glass with plus">
-        <img src="/icons/zoom-out.svg" alt="magnifying glass with minus">
-        Zoom in or out
-      </li>
-      <li>
-        <img src="/icons/dice.svg" alt="6 sided die">
-        Place a project on the map
-      </li>
-    </ul>
+      <p>
+        Anyone can draw, erase, or place projects at any time. Anyone can join the room at any time and get added to the turn order. Anyone can click <em>Pass turn</em> to end the current turn early and advance to the next player. The lack of guard rails is because this app is a facilitator, not a dungeon master. I only recommend playing with people you'd feel comfortable sitting across a table from.
+      </p>
+      <h2>Tools</h2>
+      <ul class="tools">
+        <li>
+          <img src="/icons/move-selector.svg" alt="Cross with arrows pointing in 4 directions">
+          Move the map around
+        </li>
+        <li>
+          <img src="/icons/pencil.svg" alt="pencil">
+          Draw on the map
+        </li>
+        <li>
+          <img src="/icons/eraser.svg" alt="eraser">
+          Erase on the map
+        </li>
+        <li>
+          <img src="/icons/zoom-in.svg" alt="magnifying glass with plus">
+          <img src="/icons/zoom-out.svg" alt="magnifying glass with minus">
+          Zoom in or out
+        </li>
+        <li>
+          <img src="/icons/dice.svg" alt="6 sided die">
+          Place a project on the map (hover over and click "resolve" on a project to remove the symbol)
+        </li>
+      </ul>
+    </div>
   </aside>
 {/if}
 {#if aboutVisible}
@@ -155,16 +162,18 @@
       <CancelButton onclick={() => aboutVisible = false} />
     </header>
 
-    <p>
-      <a href="https://buriedwithoutceremony.com/the-quiet-year" target="_blank">The Quiet Year</a> is created by Avery Alder. This site is in no way affiliated with or endorsed by her. You will still need the game's rules (available as a <a href="https://store.buriedwithoutceremony.com/collections/frontpage/products/the-quiet-year-pdf" target="_blank">PDF</a> or <a href="https://store.buriedwithoutceremony.com/collections/frontpage/products/the-quiet-year" target="_blank">physical media</a>) to play.
-    </p>
+    <div class="scroll">
+      <p>
+        <a href="https://buriedwithoutceremony.com/the-quiet-year" target="_blank">The Quiet Year</a> is created by Avery Alder. This site is in no way affiliated with or endorsed by her. You will still need the game's rules (available as a <a href="https://store.buriedwithoutceremony.com/collections/frontpage/products/the-quiet-year-pdf" target="_blank">PDF</a> or <a href="https://store.buriedwithoutceremony.com/collections/frontpage/products/the-quiet-year" target="_blank">physical media</a>) to play.
+      </p>
 
-    <p>
-      This app is created by <a href="https://www.toomanybees.com" target="_blank">Jess Bees</a>. The drawing mechanics were aided by a very handy <a href="https://dev.to/nyxtom/realtime-collaborative-drawing-with-canvas-and-webrtc-2d01" target="_blank">explainer by Tom Holloway</a>.
-    </p>
+      <p>
+        This app is created by <a href="https://www.toomanybees.com" target="_blank">Jess Bees</a>. The drawing mechanics were aided by a very handy <a href="https://dev.to/nyxtom/realtime-collaborative-drawing-with-canvas-and-webrtc-2d01" target="_blank">explainer by Tom Holloway</a>.
+      </p>
 
-    <p>
-      The font for dice and playing card symbols is <a href="https://speakthesky.itch.io/typeface-dicier" target="_blank">Dicier</a> by Speak the Sky, used under the <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank">CC BY 4.0 license</a>. Icons by <a href="https://www.freepik.com" target="_blank">Freepik</a> from <a href="https://www.flaticon.com/" target="_blank">www.flaticon.com</a>.
-    </p>
+      <p>
+        The font for dice and playing card symbols is <a href="https://speakthesky.itch.io/typeface-dicier" target="_blank">Dicier</a> by Speak the Sky, used under the <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank">CC BY 4.0 license</a>. Icons by <a href="https://www.freepik.com" target="_blank">Freepik</a> from <a href="https://www.flaticon.com/" target="_blank">www.flaticon.com</a>.
+      </p>
+    </div>
   </aside>
 {/if}
