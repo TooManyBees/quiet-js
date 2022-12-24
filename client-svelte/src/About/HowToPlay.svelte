fe39d9f8d5e4b47d4c4cfe98bcab01e5334a3dcb
Jess Bees <hi@toomanybees.com>
2022-12-24 13:43:11 -0700

    how to play wording

--- client-svelte/src/About/HowToPlay.svelte
+++ client-svelte/src/About/HowToPlay.svelte
@@ -16,7 +16,9 @@
   }
 </style>
 
-<p>For rules, <a href="https://store.buriedwithoutceremony.com/collections/frontpage/products/the-quiet-year-pdf" target="_blank">get a copy</a>. Before playing, bring up <a href="https://buriedwithoutceremony.com/wp-content/uploads/2019/08/The-Quiet-Year-Oracle.pdf" target="_blank">The Oracle</a> for reference.</p>
+<p>
+  For rules, <a href="https://store.buriedwithoutceremony.com/collections/frontpage/products/the-quiet-year-pdf" target="_blank">get a copy</a>. Before playing, bring up <a href="https://buriedwithoutceremony.com/wp-content/uploads/2019/08/The-Quiet-Year-Oracle.pdf" target="_blank">The Oracle</a> for reference.
+</p>
 
 <p>
   Hop on a voice chat with your friends. One person join a random room on this site, then share the link. The first person to click <em>Start game</em> takes the first turn. The game will shuffle a deck of 52 or 36 cards for you.
@@ -27,13 +29,15 @@
 </p>
 
 <p>
-  The game will handle <span class="card">KING_DIAMONDS</span> and reducing project dice on its own. It's on the players to remove projects when appropriate.
+  The game will handle <span class="card">KING_DIAMONDS</span> and reducing project dice each week on its own. But it's on the players to remove projects when appropriate.
 </p>
 
 <p>
-  Anyone can draw, erase, or place projects at any time. Anyone can join the room at any time and get added to the turn order. Anyone can click <em>Pass turn</em> to end the current turn early and advance to the next player. The lack of guard rails is because this app is a facilitator, not a dungeon master. I only recommend playing with people you'd feel comfortable sitting across a table from.
+  Anyone can draw, erase, or place projects at any time. Anyone with the right URL can join the room at any time and get added to the turn order. Anyone can click <em>Pass turn</em> to end the current turn early and advance to the next player. The lack of guard rails is because this app is a facilitator, not a dungeon master. I only recommend playing with people you'd feel comfortable sitting across a table from.
 </p>
+
 <h2>Tools</h2>
+
 <ul class="tools">
   <li>
     <img src="/icons/move-selector.svg" alt="Cross with arrows pointing in 4 directions">
